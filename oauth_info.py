22ce7833e0bd7f7ee81d1a1504ef7adf7654b590
team-voracious <60015131+team-voracious@users.noreply.github.com>
2020-01-20 21:18:37 -0600

    Add files via upload

---
+++ oauth_info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 10:48:25 2020
+
+@author: TiwarisUSA
+"""
+
+# -*- coding: latin-1 -*-
+
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+USER_NAME = '@'
